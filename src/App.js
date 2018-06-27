b4b219fbb788e99a2350f7e39ae585daf7578c76
Audrey Musselman-Brown <audrey@color.com>
2018-06-27 12:41:49 -0700

    add text field component for updating chunk text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
 import { DB } from './db.js';
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+var db = new DB()
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    db.setCurrentChunkName('Starting Chunk')
+    db.setCurrentChunkContents('some default contents')
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1> Report Creator! </h1>
+        <ReportChunkTextField />
       </div>
     );
   }
 }
+
+
+class ReportChunkTextField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: db.getCurrentChunkContents(),
+      savedContent: db.getCurrentChunkContents()
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({content: event.target.value});
+  }
+
+  handleSubmit(event) {
+    db.setCurrentChunkContents(this.state.content);
+    this.setState({savedContent: db.getCurrentChunkContents()});
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+        <div>
+          <h2> {db.getCurrentChunkName()} </h2>
+          <p> {this.state.savedContent} </p>
+        </div>
+        <label>
+          <textarea value={this.state.content} onChange={this.handleChange} />
+        <br />
+        </label>
+        <input type="submit" value="Save" />
+      </form>
+    );
+  }
+}
+
+
 
 export default App;
